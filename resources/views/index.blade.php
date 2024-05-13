5a9d7e8bc7ecac8429a2cb4d45c661a81e7561ae
ammar <ammarnazmi44@gmail.com>
2024-05-13 08:06:44 +0800

    navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,57 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>
-        index of movie
+    <div class="container mx-auto px-4 pt-10">
+        <div class="max-w-4xl mx-auto relative" x-data="{
+            activeSlide: 1,
+            slides: [
+                { id: 1, title: 'hello 1', body: 'lorem ipsum modor' },
+                { id: 2, title: 'hello 2', body: 'lorem ipsum modor' },
+                { id: 3, title: 'hello 3', body: 'lorem ipsum modor' },
+                { id: 4, title: 'hello 4', body: 'lorem ipsum modor' },
+                { id: 5, title: 'hello 5', body: 'lorem ipsum modor' },
+                { id: 6, title: 'hello 6', body: 'lorem ipsum modor' },
+            ]
+        }">
+            <!--Data Loop -->
+            <template x-for="slide in slides" :key="slide.id" >
+                <div x-show="activeSlide === slide.id"
+                    class="h-72 text-black bg-slate-200 rounded-lg overflow-hidden"
+                >
+                    <div>
+                        <img class="w-72" src="{{ asset('images/img_girl.jpg') }}" alt="Logo">
+                        <h2 class="font-bold text-2xl" x-text="slide.title"></h2>
+                        <p class="text-base" x-text="slide.body"></p>
+                    </div>
+                </div>
+            </template>
+
+            <!-- Back/next -->
+            <div class="absolute flex inset-0">
+                <div class="flex items-center justify-start w-1/2">
+                    <button
+                    x-on:click="activeSlide = activeSlide === 1 ? slides.length : activeSlide - 1"
+                    class="bg-slate-100 text-slate-500 font-bold w-10 h-10 shadow rounded-full items-center justify-center flex
+                    -ml-16 hover:bg-red-600 hover:text-white transition"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="flex items-center justify-end w-1/2">
+                    <button x-on:click="activeSlide = activeSlide === slides.length ? 1 : activeSlide + 1"
+                        class="bg-slate-100 text-slate-500 font-bold w-10 h-10 shadow rounded-full items-center justify-center flex
+                    -mr-16 hover:bg-red-600 hover:text-white transition">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,44 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>NOTFLIX</title>
+
     <link rel="stylesheet" href="/css/app.css"/>
+
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </head>
 <body class="font-sans bg-gray-950 text-white">
-    <nav class="border-b border-gray-800">
-        <div class="container mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center font-semibold">
-                <li>
-                    <a href="#">
-                        <img class="w-36" src="{{ asset('images/NotFlix.png') }}" alt="Logo">
-                    </a>
-                </li>
-                <li class="ml-16">
-                    <a class="hover:text-red-500" href="#">Movies</a>
-                </li>
-                <li class="ml-6">
-                    <a class="hover:text-red-500" href="#">TV Shows</a>
-                </li>
-                <li class="ml-6">
-                    <a class="hover:text-red-500" href="#">Actors</a>
-                </li>
-            </ul>
-
-            <div class="items-center">
-                <div class="relative">
-                    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:shadow-outline
-                     focus:outline-none text-sm" placeholder="Search"/>
-                     <div class="absolute top-0">
-                        <svg class="fill-current text-gray-500 w-5 mt-2 ml-2" viewBox="0 0 24 24">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                          </svg>
-                     </div>
-                </div>
+    <nav class="border-b border-gray-800 ">
+        <div class="container md:flex md:items-center mx-auto px-4 py-4">
+            <div class="flex justify-between items-center">
+                <a href="#">
+                    <img class="w-36" src="{{ asset('images/NotFlix.png') }}" alt="Logo">
+                </a>
+                <span class="text-3xl cursor-pointer md:hidden block">
+                    <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
+                </span>
+            </div>
+            <div class="ml-16">
+                <ul class="md:flex md:items-center z-[1] md:z-auto md:static
+                absolute right-3 w-32 md:w-auto md:py-0 gap-6 md:pl-0 pl-7
+                md:opacity-100 opacity-0 transition-all rounded-xl
+                ease-in duration-200 bg-slate-800 md:bg-inherit border-gray-800 md:border-none">
+                    <li class="my-6 md:my-0">
+                        <a class="hover:text-red-500" href="#">Movies</a>
+                    </li>
+                    <li class="my-6 md:my-0">
+                        <a class="hover:text-red-500" href="#">TV Shows</a>
+                    </li>
+                    <li class="my-6 md:my-0">
+                        <a class="hover:text-red-500" href="#">Actors</a>
+                    </li>
+                </ul>
             </div>
         </div>
-
-
     </nav>
+    <script>
+        function Menu(e) {
+          let list = document.querySelector("ul");
+
+          e.name === "menu"
+            ? ((e.name = "close"),
+              list.classList.add("opacity-95"))
+            : ((e.name = "menu"),
+              list.classList.remove("opacity-95"));
+        }
+    </script>
     @yield('content')
 </body>
 </html>
